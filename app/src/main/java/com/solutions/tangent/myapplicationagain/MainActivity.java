cbc32553398a78bea3d624807260bdbd15944107
cindymbonani <cindym@yookos.com>
2017-07-04 23:03:19 +0200

    -fyhghj

---
+++ app/src/main/java/com/solutions/tangent/myapplicationagain/MainActivity.java
@@ -0,0 +1,15 @@
+package com.solutions.tangent.myapplicationagain;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //made some hbhsbrgjrjg
+    }
+}
